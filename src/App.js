07429928d24947ca2b8c9e6091bdd5642cd22cfa
mohammadkaisarhamid <mohammadkaisarhamid@gmail.com>
2022-09-24 00:53:13 +0600

    my first react page deploying to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
   );
 }
+/* 
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+  return (
+    <div>
+      <h2>Visiting every country of the world</h2>
+      <p>Available Countries : {countries.length}</p>
+      {
+        countries.map(country => <Country name={country.name.common}></Country>)
+      }
+    </div>
+  )
+}
 
+function Country(props) {
+  return (
+    <div>
+      <h2>Name : {props.name} </h2>
+      <h3>P</h3>
+    </div>
+  )
+} */
 export default App;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    const { name, population, area, region, flags } = props.country;
+    console.log(props.country)
+    return (
+        <div className="country">
+
+            <h2>Country Name:{name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population:{population}</p>
+            <p>Area:{area}</p>
+            <p>Region:{region}</p>
+        </div>
+    );
+};
+
+export default Country;
